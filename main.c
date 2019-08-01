515edd36757c7574c6dac9673d54e8b5ae783d18
DenisovGL <denisov0gleb@gmail.com>
2019-08-01 20:17:25 +0300

    Add Line struct [dgl]

--- main.c
+++ main.c
@@ -11,42 +11,43 @@ typedef struct Point
 } Point;
 
 
+typedef struct Line
+{
+	/*
+	Point start;
+	Point end;
+	*/
+	int x0;
+	int y0;
+	int x1;
+	int y1;
+} Line;
+
+
 int main(int argc, const char *argv[])
 {
 	const int Ximg = 100;
 	const int Yimg = 100;
+ 	int x, y;
 
 	Color * black = CreateColor(0, 0, 0);
 	Color * red = CreateColor(255, 0, 0);
 	Color * white = CreateColor(255, 255, 255);
 	Color * blue = CreateColor(0, 0, 255);
 
-	double tan;
-	Point start;
-	Point end;
+
+	Line line1 = {.x0 = 1, .y0 = 50, .x1 = 70, .y1 = 50};
+
+	double tan = (double) (line1.y1 - line1.y0) / (line1.x1 - line1.x0);
 
 	ppmImg * img = CreateImg(Ximg, Yimg, 255);
-	//img->format = "P3";
-
 	SetBackgroundColor(img, white);
-	SetPixelColor(img, Ximg, Yimg, black);
 
 
-	start.x = 1;
-	start.y = 4;
-	end.x = Ximg-5;
-	end.y = 4;
-
-	tan = (double) (end.y - start.y) / (end.x - start.x);
-
-	//fprintf(stdout, "tan = %f\n", tan);
-
-	int x, y;
-	for (x = start.x; x <= end.x; x++)
+	for (x = line1.x0; x <= line1.x1; x++)
 	{
-		y = round( tan * (x - start.x) + start.y );
+		y = round( tan * (x - line1.x0) + line1.y0 );
 
-		//fprintf(stdout, "x = %d, y = %d\n", x, y);
 		SetPixelColor(img, x, Yimg - y, red);
 	}
 
