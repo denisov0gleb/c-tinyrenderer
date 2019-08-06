9a1a849fed32fb97d720fb7b741605b1e079d08b
DenisovGL <denisov0gleb@gmail.com>
2019-08-06 18:18:10 +0300

    Add line function [dgl]

--- main.c
+++ main.c
@@ -13,10 +13,6 @@ typedef struct Point
 
 typedef struct Line
 {
-	/*
-	Point start;
-	Point end;
-	*/
 	int x0;
 	int y0;
 	int x1;
@@ -24,24 +20,38 @@ typedef struct Line
 } Line;
 
 
-void swapLinePoints(Line * line)
+void swapInt(int * a, int * b)
 {
-	int temp = line->x0;
-	int temp1 = line->y0;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-	line->x0 = line->x1;
-	line->y0 = line->y1;
 
-	line->x1 = temp;
-	line->y1 = temp1;
+void line(int x0, int y0, int x1, int y1, ppmImg * img, Color * color)
+{
+	int x, y;
+
+	double tan = (double) (y1 - y0) / (x1 - x0);
+	if (x0 > x1)
+	{
+		swapInt(&x0, &x1);
+		swapInt(&y0, &y1);
+	}
+
+	for (x = x0; x <= x1; x++)
+	{
+		y = round( tan * (x - x0) + y0 );
+
+		SetPixelColor(img, x, img->height - y, color);
+	}
 }
 
 
 int main(int argc, const char *argv[])
 {
-	const int Ximg = 100;
-	const int Yimg = 100;
-	int x, y;
+	const int imgWidth = 100;
+	const int imgHeight = 100;
 
 	Color * black = CreateColor(0, 0, 0);
 	Color * red = CreateColor(255, 0, 0);
@@ -49,27 +59,15 @@ int main(int argc, const char *argv[])
 	Color * blue = CreateColor(0, 0, 255);
 
 
-	Line line1 = {.x0 = 50, .y0 = 70, .x1 = 30, .y0 = 30};
-	Line line2 = {.x0 = 1, .y0 = 50, .x1 = 70, .y1 = 50};
+	Line line1 = {.x0 = 30, .y0 = 30, .x1 = 50, .y1 = 70};
+	Line line2 = {.x0 = 1, .y0 = 50, .x1 = 70, .y1 = 60};
 
-	double tan = (double) (line1.y1 - line1.y0) / (line1.x1 - line1.x0);
 
-	ppmImg * img = CreateImg(Ximg, Yimg, 255);
+	ppmImg * img = CreateImg(imgWidth, imgHeight, 255);
 	SetBackgroundColor(img, white);
 
-
-	if (line1.x0 > line1.x1)
-	{
-		swapLinePoints(&line1);
-	}
-
-
-	for (x = line1.x0; x <= line1.x1; x++)
-	{
-		y = round( tan * (x - line1.x0) + line1.y0 );
-
-		SetPixelColor(img, x, Yimg - y, red);
-	}
+	line(line1.x0, line1.y0, line1.x1, line1.y1, img, red);
+	line(line2.x0, line2.y0, line2.x1, line2.y1, img, blue);
 
 	WriteImgToFile(img, "image.ppm");
 
